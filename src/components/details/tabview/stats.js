e656112d425ee000618b78a499bd76b3e693f541
Shemil Hashan <shemil123@gmail.com>
2021-01-26 03:42:28 -0500

    stats tab

---
+++ src/components/details/tabview/stats.js
@@ -0,0 +1,118 @@
+import React from 'react';
+import { View, Text, Image, StyleSheet, Dimensions, } from 'react-native';
+const styles = StyleSheet.create({
+    scene: {
+        flex: 1,
+    },
+    tile: {
+        marginTop: 20,
+        marginBottom: 10,
+        width: '90%',
+        height: 100,
+        borderRadius: 20,
+        marginRight: '5%',
+        marginLeft: '5%',
+        shadowColor: 'rgba(0,0,0, .4)', // IOS
+        shadowOffset: { height: 0, width: 1 }, // IOS
+        shadowOpacity: 1, // IOS
+        shadowRadius: 1, //IOS
+        elevation: 4, // Android,
+        overflow: 'hidden',
+        justifyContent: 'center',
+        backgroundColor: 'white',
+        flexDirection: 'row',
+    },
+});
+function Stats(props) {
+    const {allData,speciesData,currentId} = props
+    let dataLoaded = allData[currentId - 1];
+    let speciesLoaded = speciesData[currentId - 1];
+    function getProgress(stat, total) {
+        return (
+            <View style={{ width: '100%', height: 4, flexDirection: 'row', borderRadius: 2, overflow: 'hidden', }}>
+                <View style={{ flex: stat, height: 4, backgroundColor: stat / total >= 0.5 ? 'green' : 'red', }} />
+                <View style={{ flex: total - stat, height: 4, backgroundColor: 'grey', }} />
+            </View>
+        );
+    }
+    const baseList = dataLoaded.stats.map((stat) => {
+        return (
+            <View style={{ flexDirection: 'row', paddingLeft: 20, marginTop: 5, marginBottom: 5, }}>
+                <Text style={{ flex: 4, textAlign: 'left', fontWeight: 'bold', marginRight: 5, color: 'grey', }}>
+                    {stat.name}
+                </Text>
+                <Text style={{ flex: 1, textAlign: 'left', fontWeight: 'bold', marginRight: 5, color: 'grey', }}>
+                    {stat.value}
+                </Text>
+                <View style={{ flex: 8, justifyContent: 'center', alignItems: 'center', alignContent: 'center', }}>
+                    {getProgress(stat.value, 100)}
+                </View>
+                <View style={{ width: 20 }} />
+            </View>
+        );
+    });
+
+    function otherStat(name, value) {
+        return (
+            <View style={{ flexDirection: 'row', paddingLeft: 20, marginTop: 5, marginBottom: 5, }}>
+                <Text style={{ flex: 4, textAlign: 'left', fontWeight: 'bold', marginRight: 5, color: 'grey', }}>
+                    {name}
+                </Text>
+                <Text style={{ flex: 9, textAlign: 'left', fontWeight: 'bold', marginRight: 5, color: 'grey', }}>
+                    {value}
+                </Text>
+                <View style={{ width: 20, }}
+                />
+            </View>
+        );
+    }
+
+    function total() {
+        let statArr = dataLoaded.stats;
+        var totalToDivide = 0;
+        var total = 0;
+
+        for (let i = 0; i < statArr.length; i++) {
+            totalToDivide += 100;
+            total += statArr[i].value;
+        }
+
+        return (
+            <View style={{ flexDirection: 'row', paddingLeft: 20, marginTop: 5, marginBottom: 5, }}>
+                <Text style={{ flex: 4, textAlign: 'left', fontWeight: 'bold', marginRight: 5, color: 'grey', }}>
+                    Total
+          </Text>
+                <Text style={{ flex: 1, textAlign: 'left', fontWeight: 'bold', marginRight: 5, color: 'grey', }}>
+                    {total}
+                </Text>
+                <View style={{ flex: 8, justifyContent: 'center', alignItems: 'center', alignContent: 'center', }}>
+                    {getProgress(total, totalToDivide)}
+                </View>
+                <View style={{ width: 20, }} />
+            </View>
+        );
+    }
+
+    return (
+        <View style={[styles.scene, { backgroundColor: 'white', },]}>
+            <Text style={{ paddingLeft: 20, marginBottom: 10, marginTop: 20, fontWeight: 'bold', fontSize: 17, }}>
+                Base Stats
+        </Text>
+            {baseList}
+            {total()}
+            <Text style={{ paddingLeft: 20, marginBottom: 10, marginTop: 20, fontWeight: 'bold', fontSize: 17, }}>
+                Other Stats
+        </Text>
+            {otherStat('Experience', dataLoaded.base_experience)}
+            {otherStat('Happiness', speciesLoaded.base_happiness)}
+            {otherStat('Capture Rate', speciesLoaded.capture_rate)}
+            {otherStat('Generation', speciesLoaded.generation.capitalize())}
+            {otherStat('Growth Rate', speciesLoaded.growth_rate.capitalize())}
+            {otherStat('Habitat', speciesLoaded.habitat.capitalize())}
+        </View>
+    );
+}
+
+export default Stats;
+
+
--- src/scenes/detailsalt.js
+++ src/scenes/detailsalt.js
@@ -19,6 +19,7 @@ import images from '../assets/images';
 import CachedImage from '../components/cacheimage';
 import { TabView, SceneMap, TabBar } from 'react-native-tab-view';
 import About from '../components/details/tabview/about'
+import Stats from '../components/details/tabview/stats'
 const initialLayout = { width: Dimensions.get('window').width };
 
 const styles = StyleSheet.create({
@@ -174,98 +175,14 @@ export default function DetailAltScreen({ navigation, route }) {
     <About allData={allData} speciesData={speciesData} currentId={currentId}></About>
   );
 
-  function Stats() {
-    let dataLoaded = allData[currentId - 1];
-    let speciesLoaded = speciesData[currentId - 1];
-    const baseList = dataLoaded.stats.map((stat) => {
-      return (
-        <View style={{ flexDirection: 'row', paddingLeft: 20, marginTop: 5, marginBottom: 5, }}>
-          <Text style={{ flex: 4, textAlign: 'left', fontWeight: 'bold', marginRight: 5, color: 'grey', }}>
-            {stat.name}
-          </Text>
-          <Text style={{ flex: 1, textAlign: 'left', fontWeight: 'bold', marginRight: 5, color: 'grey', }}>
-            {stat.value}
-          </Text>
-          <View style={{ flex: 8, justifyContent: 'center', alignItems: 'center', alignContent: 'center', }}>
-            {getProgress(stat.value, 100)}
-          </View>
-          <View style={{ width: 20 }} />
-        </View>
-      );
-    });
-
-    function otherStat(name, value) {
-      return (
-        <View style={{ flexDirection: 'row', paddingLeft: 20, marginTop: 5, marginBottom: 5, }}>
-          <Text style={{ flex: 4, textAlign: 'left', fontWeight: 'bold', marginRight: 5, color: 'grey', }}>
-            {name}
-          </Text>
-          <Text style={{ flex: 9, textAlign: 'left', fontWeight: 'bold', marginRight: 5, color: 'grey', }}>
-            {value}
-          </Text>
-          <View style={{ width: 20, }}
-          />
-        </View>
-      );
-    }
-
-    function total() {
-      let statArr = dataLoaded.stats;
-      var totalToDivide = 0;
-      var total = 0;
-
-      for (let i = 0; i < statArr.length; i++) {
-        totalToDivide += 100;
-        total += statArr[i].value;
-      }
-
-      return (
-        <View style={{ flexDirection: 'row', paddingLeft: 20, marginTop: 5, marginBottom: 5, }}>
-          <Text style={{ flex: 4, textAlign: 'left', fontWeight: 'bold', marginRight: 5, color: 'grey', }}>
-            Total
-          </Text>
-          <Text style={{ flex: 1, textAlign: 'left', fontWeight: 'bold', marginRight: 5, color: 'grey', }}>
-            {total}
-          </Text>
-          <View style={{ flex: 8, justifyContent: 'center', alignItems: 'center', alignContent: 'center', }}>
-            {getProgress(total, totalToDivide)}
-          </View>
-          <View style={{ width: 20, }} />
-        </View>
-      );
-    }
-
-    return (
-      <View style={[styles.scene, { backgroundColor: 'white', },]}>
-        <Text style={{ paddingLeft: 20, marginBottom: 10, marginTop: 20, fontWeight: 'bold', fontSize: 17, }}>
-          Base Stats
-        </Text>
-        {baseList}
-        {total()}
-        <Text style={{ paddingLeft: 20, marginBottom: 10, marginTop: 20, fontWeight: 'bold', fontSize: 17, }}>
-          Other Stats
-        </Text>
-        {otherStat('Experience', dataLoaded.base_experience)}
-        {otherStat('Happiness', speciesLoaded.base_happiness)}
-        {otherStat('Capture Rate', speciesLoaded.capture_rate)}
-        {otherStat('Generation', speciesLoaded.generation.capitalize())}
-        {otherStat('Growth Rate', speciesLoaded.growth_rate.capitalize())}
-        {otherStat('Habitat', speciesLoaded.habitat.capitalize())}
-      </View>
-    );
-  }
+  const StatsTab = () => (
+    <Stats allData={allData} speciesData={speciesData} currentId={currentId}></Stats>
+  )
 
   const imgCommon =
     'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/';
 
-  function getProgress(stat, total) {
-    return (
-      <View style={{ width: '100%', height: 4, flexDirection: 'row', borderRadius: 2, overflow: 'hidden', }}>
-        <View style={{ flex: stat, height: 4, backgroundColor: stat / total >= 0.5 ? 'green' : 'red', }} />
-        <View style={{ flex: total - stat, height: 4, backgroundColor: 'grey', }} />
-      </View>
-    );
-  }
+  
 
   function Evolution() {
     let evoArr = evoData[speciesData[currentId - 1].evoID];
@@ -400,7 +317,7 @@ export default function DetailAltScreen({ navigation, route }) {
 
   const renderScene = SceneMap({
     about: AboutTab,
-    stats: Stats,
+    stats: StatsTab,
     evolution: Evolution,
   });
 
